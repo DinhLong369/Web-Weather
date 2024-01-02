6ea0bf548aec8b5c32dfd17eefb5ee23884b9c9f
DinhLong369 <dodinhlong369@gmail.com>
2024-01-02 19:46:52 +0700

    project

---
+++ ASP.NET-WEB_API-DuBaoThoiTiet-main/NewsServices/News.js
@@ -0,0 +1,32 @@
+﻿function HotNews() {
+    $.ajax({
+        url: "https://newsapi.org/v2/top-headlines?country=us&apiKey=b38ccf3fc2a8464ca490114081bfe456&category=general",
+        method: "GET",
+        dataType: "json",
+        success: function (data) {
+            var news_ul = document.getElementById('hot_news');
+            // Xử lí dữ liệu
+            for (let i = 0; i < 3; i++) {
+                //lấy thông tin tin tức 
+                var title = data.articles[i].title;
+                var imageUrl = data.articles[i].urlToImage;
+                //tạo chuỗi html để hiển thị tin tức
+                str = `<li class="media my-1 pb-2 list-top-new-items">
+                            <img src="${imageUrl}" class="me-3">
+                            <div class="media-body">
+                                <h5 class="mt-0 mb-1">${title} </h5>
+                            </div>
+                            <a href="/TinTuc/Detail?id=${i}" class="top-news-link"></a>
+                        </li>`;
+                //thêm chuỗi html vào danh sách tin tức
+                news_ul.insertAdjacentHTML('beforeend', str);
+            }
+        },
+        error: function () {
+            alert("Không thể lấy News!");
+        }
+    });
+}
+$(document).ready(function () {
+    HotNews();
+})
